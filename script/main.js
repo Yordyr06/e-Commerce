9809e993fe193b39c9b179390aac34c3d279c6d6
Yordy <yoalmonte.dev@outlook.com>
2022-09-26 22:15:33 -0400

    inicio de main.js

---
+++ script/main.js
@@ -0,0 +1,8 @@
+const navbarEmail = document.querySelector('.navbar-email');
+const activeMenu = document.querySelector('.sing-out-menu');
+
+navbarEmail.addEventListener('click', toggleEmailMenu);
+
+function toggleEmailMenu() {
+    activeMenu.classList.toggle('inactive');
+}
