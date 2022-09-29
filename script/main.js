c51eb0da20c46798181a04c3d0bf7b3b2edb6fa8
Yordy <yoalmonte.dev@outlook.com>
2022-09-28 23:04:51 -0400

    Agregado mobile-menual a los diferentes archivos main

--- script/main.js
+++ script/main.js
@@ -1,8 +1,15 @@
-const navbarEmail = document.querySelector('.navbar-email')
+const navbarEmail = document.querySelector('.navbar-email');
 const activeMenu = document.querySelector('.sing-out-menu');
+const iconMenu = document.querySelector('.icon-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleEmailMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleEmailMenu() {
     activeMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
